c6df7d9c2bfbd751836bebe97fa10454e5731b39
JaasiyahM01 <105018778+JaasiyahM01@users.noreply.github.com>
2024-08-01 18:30:27 -0400

    commit

---
+++ HairBookingApplication.java
@@ -0,0 +1,12 @@
+package com.example.hairbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HairBookingApplication.class, args);
+    }
+}
