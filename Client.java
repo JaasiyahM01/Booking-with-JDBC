77b40c06d2945d6ddb27235337045a4b653b44ac
JaasiyahM01 <105018778+JaasiyahM01@users.noreply.github.com>
2024-08-01 18:27:18 -0400

    commit

---
+++ Client.java
@@ -0,0 +1,12 @@
+package com.example.hairbooking;
+
+public class Client {
+    private int id;
+    private String name;
+    private String hairService;
+    private String appointmentDate;
+    private String phoneNumber;
+    private String address;
+
+   
+}
